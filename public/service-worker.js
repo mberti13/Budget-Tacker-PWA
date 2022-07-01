8949edc172e8dfd7c74b363d8895e12627f473cc
MJB <matt.berti13@gmail.com>
2022-07-01 17:22:54 -0500

    added service worker instructions

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,8 +2,8 @@
     const FILES_TO_CACHE = [
         './index.html',
         "./js/index.js",
-        "./css/styles.css"
-        //TODO: Add idb.js
+        "./css/styles.css",
+        "./js/idb.js"
     ]
 
 const APP_PREFIX = "BudgetTracker-";
@@ -19,6 +19,27 @@ self.addEventListener('install', function(e){
         caches.open(CACHE_NAME).then(function(cache){
             console.log('installing cache : ' + CACHE_NAME)
             return cache.addAll(FILES_TO_CACHE)
+        })
+    )
+});
+
+self.addEventListener('activate', function(e){
+    e.waitUntil(
+        caches.keys().then(function(keyList){
+            let cacheKeepList = keyList.filter(function(key){
+                return key.indexOf(APP_PREFIX);
+            })
+            cacheKeepList.push(CACHE_NAME);
+
+
+            return Promise.all(
+                keyList.map(function(key, i){
+                    if(cacheKeepList.indexOf(key) === -1){
+                        console.log('deleting cache: ' + keyList[i]);
+                        return caches.delete(keyList[i]);
+                    }
+                })
+            );
         })
     )
 });
