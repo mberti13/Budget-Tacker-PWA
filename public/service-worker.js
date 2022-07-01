c31feab42549ffe5682b048f8bbcc52d6ca4ac37
MJB <matt.berti13@gmail.com>
2022-07-01 17:41:31 -0500

    added working service weorkers

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,6 @@
     // Files to cache in browser
     const FILES_TO_CACHE = [
-        './index.html',
+        "./index.html",
         "./js/index.js",
         "./css/styles.css",
         "./js/idb.js"
@@ -43,3 +43,21 @@ self.addEventListener('activate', function(e){
         })
     )
 });
+
+self.addEventListener('fetch', function(e){
+    console.log('fetch request: ' +  e.request.url)
+    e.respondWith(
+        caches.match(e.request).then(function(request){
+            if(request){
+                console.log('responding with cache: ' + e.request.url)
+                return request
+            }else{   // if there are no cache, try fetching request
+                console.log('file is not cached, fetching: ' + e.request.url)
+                return fetch(e.request)
+            }
+
+            // You can omit if/else for console.log & put one line below like this too.
+            // return request || fetch(e.request)
+        })
+    )
+})
