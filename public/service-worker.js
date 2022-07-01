e201442558c35ecb6731638faa8a9a85160f9c39
MJB <matt.berti13@gmail.com>
2022-07-01 16:21:35 -0500

    wrote code for service worker

---
+++ public/service-worker.js
@@ -0,0 +1,24 @@
+    // Files to cache in browser
+    const FILES_TO_CACHE = [
+        './index.html',
+        "./js/index.js",
+        "./css/styles.css"
+        //TODO: Add idb.js
+    ]
+
+const APP_PREFIX = "BudgetTracker-";
+const VERSION = "version_01";
+const CACHE_NAME = APP_PREFIX + VERSION;
+
+
+
+
+// Self because service workers run before browser window 
+self.addEventListener('install', function(e){
+    e.waitUntil(
+        caches.open(CACHE_NAME).then(function(cache){
+            console.log('installing cache : ' + CACHE_NAME)
+            return cache.addAll(FILES_TO_CACHE)
+        })
+    )
+});
