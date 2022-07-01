7a4f168d151682aae8b47989d8641b1e6915e703
MJB <matt.berti13@gmail.com>
2022-07-01 17:14:28 -0500

    finished offline IndexedDB functionality and storage

--- public/js/idb.js
+++ public/js/idb.js
@@ -21,7 +21,7 @@ request.onsuccess = function(event){
     //check if app is online, if yes run uploadTransaction() function to send all local db bata to api
     if(navigator.onLine){
         //coding soon
-        // uploadTransaction();
+        uploadTransaction();
     }
 };
 
@@ -41,4 +41,50 @@ function saveTransaction(record){
 
     // add a record to your store with add method
     transactionObjectStore.add(record);
+};
+
+function uploadTransaction(){
+    // open a transaction on your db
+    const transaction = db.transaction(['new_transaction'], 'readwrite');
+
+    //access your object store
+    const transactionObjectStore = transaction.objectStore('new_transaction');
+
+    //get all records from store and set to a variable
+    const getAll = transactionObjectStore.getAll();
+
+    //upon a successful .getAll() execution, run this function
+    getAll.onsuccess = function(){
+        // if there was data in indexedDb's store, lets send it to the api server
+        if(getAll.result.length > 0){
+            fetch('/api/transaction', {
+                method: 'POST',
+                body: JSON.stringify(getAll.result),
+                headers:{
+                    Accept: 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json'
+                }
+            })
+            .then(response => response.json())
+            .then(serverResponse => {
+                if(serverResponse.message){
+                    throw new Error(serverResponse);
+                }
+                // open one more transaction
+                const transaction = db.transaction(['new_transaction'], 'readwrite');
+                //access new_transaction object store
+                const transactionObjectStore = transaction.objectStore('new_transaction');
+                //clear all items in your store
+                transactionObjectStore.clear();
+
+                alert('All transactions have been submitted!')
+            })
+            .catch(err =>{
+                console.log(err);
+            });
+        }
+    }
 }
+
+// listen for app coming back online
+window.addEventListener('online', uploadTransaction);
